b2753622646fdc1480cf33cb89d6889d16b8d164
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2022-06-07 08:30:57 +0800

    add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[70vh]">
+      <h1 className="text-5xl font-bold text-gray-800">404</h1>
+      <p className="text-xl text-gray-600">Not Found</p>
+    </div>
+  )
+}
